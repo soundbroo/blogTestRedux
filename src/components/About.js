68a9be6d1048bffef393924964f000868308c75a
soundbroo <soundbroo@gmail.com>
2019-05-01 18:41:44 +0300

    Adaptive design

--- src/components/About.js
+++ src/components/About.js
@@ -2,11 +2,6 @@ import React from 'react';
 import './About.css';
 import { Loader } from './Loader'
 
-const About = () => (
-    <div>
-        <div className="About">Only the infinite loader</div>
-        <Loader />
-    </div>
-)
+const About = () => (<Loader />)
 
 export default About;
